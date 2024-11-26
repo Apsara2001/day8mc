a0311a628fff67c875501946b60c17959f97fcca
Apsara2001 <139642058+Apsara2001@users.noreply.github.com>
2024-11-26 08:53:58 +0000

    updating app.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -10,7 +10,13 @@ function App() {
   return (
     <div className="App">
      <Router>
-      
+     <nav>
+        <ul>
+          <li><Link to="/home">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
       <Routes>
         
           <Route path='/home' element={<Home/>}/>
