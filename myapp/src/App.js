a70df256f2d7061f5b9104b3d409a26bf77edda5
Apsara2001 <139642058+Apsara2001@users.noreply.github.com>
2024-11-26 08:53:30 +0000

    Adding content to App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
+
 import './App.css';
+import {BrowserRouter as Router,Routes,Route,Link,NavLink} from 'react-router-dom';
+import Home from './componets/Home'
+import AboutUs from './componets/AboutUs'
+import ContactUs from './componets/ContactUs'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Router>
+      
+      <Routes>
+        
+          <Route path='/home' element={<Home/>}/>
+          <Route path='/about' element={<AboutUs/>}/>
+          <Route path='/contact' element={<ContactUs/>}/>
+        
+      </Routes>
+     </Router>
     </div>
   );
 }
