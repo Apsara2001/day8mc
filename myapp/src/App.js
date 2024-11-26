b5aba901dc6b3a5d55fd5120d3df669f64134a62
Apsara2001 <139642058+Apsara2001@users.noreply.github.com>
2024-11-26 09:10:35 +0000

    Adding Dashboard and Login componts

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,9 +2,9 @@ import logo from './logo.svg';
 
 import './App.css';
 import {BrowserRouter as Router,Routes,Route,Link,NavLink} from 'react-router-dom';
-import Home from './componets/Home'
-import AboutUs from './componets/AboutUs'
-import ContactUs from './componets/ContactUs'
+import Home from './components/Home'
+import AboutUs from './components/AboutUs'
+import ContactUs from './components/ContactUs'
 
 function App() {
   return (
